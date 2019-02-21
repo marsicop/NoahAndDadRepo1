b6d5b21a825419c92a30cea5ff4f5bf094ee3645
marsicop <marsicop@gmail.com>
2019-02-21 11:25:58 -0500

    Dad's update

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-var name = 'Joe Blow';
-console.log(name);
+// initialization
+let runningTotal = 0;
+
+function getNumber(id) {
+  runningTotal = runningTotal + id;
+  console.log(runningTotal);
+}
+
+function addNumbers() {
+
+}
