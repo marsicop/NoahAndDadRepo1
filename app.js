3237bf1c1a89b09ee0c7c29c91e3e73f538ddee8
MarsicoN <marsicong@gmail.com>
2019-02-21 12:23:59 -0500

    Noahs 1st update

--- app.js
+++ app.js
@@ -1,2 +1,45 @@
-var name = 'Joe Blow';
-console.log(name);
+let RunningTotal = 0;  
+let state;
+let Firstn = 0;
+function getNumber(id){
+    console.log(id);
+    if(state == "+"){
+        console.log(Firstn + id);
+        state = 0;
+    } else if (state == "-") {
+        console.log(Firstn - id);
+        state = 0;
+    } else if (state == "*"){
+        console.log(Firstn * id);
+        state = 0;
+    } else if (state == "/"){
+        console.log(Firstn / id);
+        state = 0;
+    }
+
+    Firstn = id;
+}
+
+function add(){
+    console.log("+");
+    state = "+";
+}
+
+function subt(){
+    console.log("-");
+    state = "-";
+}
+
+function mult(){
+    console.log("*");
+    state = "*";
+}
+
+function div(){
+    console.log("/");
+    state = "/";
+}
+
+function AC(){
+    console.clear();
+}
