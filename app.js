c41e865b07b4d2870d372d77798f11e9d26962f6
MarsicoN <47108395+MarsicoN@users.noreply.github.com>
2019-03-01 11:34:43 -0500

    2nd File

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 let RunningTotal = 0;
 let state;
 let Firstn = 0;
+let ANS;
 function getNumber(id) {
     console.log(id);
+    if(id == "ANS"){
+        id = ANS;
+    }
+    document.getElementById("answer_element").innerHTML = id;
     if (state == "+") {
         console.log(Firstn + id);
-        state = 0;
+        document.getElementById("answer_element").innerHTML = Firstn + " + " + id ;
+        document.getElementById("answer_element").innerHTML = Firstn + " + " + id + " = " + (Firstn + id);
+        ANS = Firstn + id;
+        state = 0; 
     } else if (state == "-") {
         console.log(Firstn - id);
+        document.getElementById("answer_element").innerHTML = Firstn + " - " + id ;
+        document.getElementById("answer_element").innerHTML = Firstn + " - " + id + " = " + (Firstn - id);
+        ANS = Firstn - id;
         state = 0;
     } else if (state == "*") {
         console.log(Firstn * id);
+        document.getElementById("answer_element").innerHTML = Firstn + " * " + id ;
+        document.getElementById("answer_element").innerHTML = Firstn + " * " + id + " = " + (Firstn * id);
+        ANS = Firstn * id;
         state = 0;
     } else if (state == "/") {
         console.log(Firstn / id);
+        document.getElementById("answer_element").innerHTML = Firstn + " / " + id ;
+        document.getElementById("answer_element").innerHTML = Firstn + " / " + id + " = " + (Firstn / id);
+        ANS = Firstn / id;
+        state = 0;
+    } else if (state == "^x") {
+        console.log(Math.pow(Firstn, id));
+        document.getElementById("answer_element").innerHTML = Firstn + "^" + id ;
+        document.getElementById("answer_element").innerHTML = Firstn + "^" + id + " = " + Math.pow(Firstn, id);
+        ANS = Math.pow(Firstn, id);
         state = 0;
     }
 
@@ -24,31 +47,45 @@ function getNumber(id) {
     //  if you look in the html file, you'll find an element that i created named
     //  answer_space.  the code below grabs this element and sets the html text that 
     //  is contained in within the tag.
-    document.getElementById("answer_element").innerHTML = "the answer";
+    
 }
 
 function add() {
     console.log("+");
+    document.getElementById("answer_element").innerHTML = Firstn + " + ";
     state = "+";
 }
 
 function subt() {
     console.log("-");
+    document.getElementById("answer_element").innerHTML = Firstn + " - ";
     state = "-";
 }
 
 function mult() {
     console.log("*");
+    document.getElementById("answer_element").innerHTML = Firstn + " * ";
     state = "*";
 }
 
 function div() {
     console.log("/");
+    document.getElementById("answer_element").innerHTML = Firstn + " / ";
     state = "/";
 }
 
 function AC() {
     console.clear();
+}
+
+function pow(){
+    console.log("^x");
+    document.getElementById("answer_element").innerHTML = Firstn + "^ ";
+    state = "^x";
+}
+
+function Ans(){
+    id = ANS;
 }
 
 
